76496d11674179b26eacd57a62ecb0a6128459a9
Blair Gosselin <blairgosselin@blairgosselin-macbookpro.roam.corp.google.com>
2018-08-21 17:53:35 -0400

    Min Viable Version

--- BackEnd/BackEnd.py
+++ BackEnd/BackEnd.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
+def call_back_end_service():
+    return 'hello world'
+
 @app.route("/", methods=['GET'])
 def hello():
-    return "Hello World!"
+    return call_back_end_service()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5002"), debug=False)
--- FrontEnd/FrontEnd.py
+++ FrontEnd/FrontEnd.py
@@ -1,10 +1,23 @@
-from flask import Flask, request
+from flask import Flask
+import requests
+
+
+URL = 'helloworld-backend-service'
 
 app = Flask(__name__)
 
+def call_back_end_service():
+    resp = requests.get('http://'+URL+':5002', verify=False)
+    if resp.status_code == 200:
+        response = resp.content
+    else:
+        response = 'Fail'
+    return response
+
 @app.route("/", methods=['GET'])
 def hello():
-    return "Hello World!"
+    return call_back_end_service()
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("5002"), debug=False)
+    app.run(host="0.0.0.0", port="80")
